3cb6cc591e7891d6d5c99df01de95d2ef43ad58b
Armisuari <87459923+Armisuari@users.noreply.github.com>
2021-12-04 16:16:42 +0800

    Add files via upload

---
+++ eca/4_channel_Relay_Android.ino
@@ -0,0 +1,78 @@
+#include <SoftwareSerial.h> 
+#include <Wire.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <LiquidCrystal_I2C.h>
+
+#define ONE_WIRE_BUS 6
+
+SoftwareSerial module_bluetooth(0, 1); // pin RX | TX
+OneWire oneWire (ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+LiquidCrystal_I2C lcd(0x27,16,2);
+
+float suhuAtas, suhuBawah;
+char data = 0;
+            
+void setup() {
+  Serial.begin(9600);     
+  lcd.init(); 
+  lcd.backlight();     
+  pinMode(2, OUTPUT);  //inisialisasi PIN 2 Menjadi Output
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  digitalWrite(2, HIGH); // pada kondisi awal dibuat high supaya relay mati (karena modul relay aktif low)
+  digitalWrite(3, HIGH);
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH); 
+}
+
+void loop(){
+  sensors.setResolution(10);
+  sensors.requestTemperatures();
+  suhuAtas  = (sensors.getTempCByIndex(0));
+  suhuBawah = (sensors.getTempCByIndex(1));
+
+    if(suhuAtas >= 20){
+    digitalWrite(2, LOW); //nyalakan relay
+    }
+    else if(suhuAtas <= 16){      
+    digitalWrite(2, HIGH); //matikan relay
+    } 
+    else if(suhuBawah <= 45){
+    digitalWrite(3, LOW); 
+    }
+    else if(suhuBawah >= 50){      
+    digitalWrite(3, HIGH); 
+    } 
+   
+  if(Serial.available() > 0)  
+  {
+    data = Serial.read(); //baca data yang dikirim      
+    if(data == '5'){
+    digitalWrite(4, LOW); 
+    }
+    else if(data == '6'){      
+    digitalWrite(4, HIGH); 
+    } 
+    else if(data == '7'){
+    digitalWrite(5, LOW); 
+    }
+    else if(data == '8'){      
+    digitalWrite(5, HIGH); 
+    } 
+  }
+   lcd.setCursor(0,0);
+   lcd.print("SuhuA:");
+   lcd.setCursor(7,0);
+   lcd.print(suhuAtas);
+   lcd.setCursor(10,0);
+   lcd.print(" C");
+   lcd.setCursor(0,1);
+   lcd.print("SuhuB:");
+   lcd.setCursor(7,1);
+   lcd.print(suhuBawah);
+   lcd.setCursor(11,1);
+   lcd.print(" C");   
+ }
